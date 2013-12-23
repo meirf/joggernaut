05fbf329dfe899f4b15f036262f635a2457700a2
Meir <meirfischer@gmail.com>
2013-12-23 15:43:01 -0500

    added graph models to db

--- polls/models.py
+++ polls/models.py
@@ -4,16 +4,21 @@ import datetime
 from django.utils import timezone
 
 class Node(models.Model):
-    node_id = models.CharField(max_length=200)
-    latit = models.DateTimeField('date published')
-    longit
-    elevation
+    node_id = models.IntegerField()
+    latit = models.FloatField()
+    longit = models.FloatField()
+    elevation = models.FloatField()
 
     def __unicode__(self):
         return self.node_id
 
+class Edge(models.Model):
+    node_id_a = models.ForeignKey(Node, related_name='from_node')
+    node_id_b = models.ForeignKey(Node, related_name='to_node')
+    distance = models.FloatField()
 
-
+    def __unicode__(self):
+        return "{" + str(self.node_id_a) + " -> " + str(self.node_id_b) + "}"
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
