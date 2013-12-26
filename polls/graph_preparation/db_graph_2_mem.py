f7986150f27461502b29a2ae7b4c067c39b990b8
Meir <meirfischer@gmail.com>
2013-12-26 13:22:24 -0500

    check that a source node has been chosen and not set to default

--- polls/graph_preparation/db_graph_2_mem.py
+++ polls/graph_preparation/db_graph_2_mem.py
@@ -3,18 +3,29 @@ __author__ = 'meirfischer'
 from collections import defaultdict
 from polls.models import Node, Edge
 
-import pdb
-
 def get_elevs():
     elevs = [None] * (len(Node.objects.all()) + 1)
     for node in Node.objects.all():
         elevs[node.node_id] = node.elevation
     return elevs
 
+
+"""Not identical to original adjacency list
+   use in graph_input_2_db.py, which was
+        dictionary mapping each node id to a list,
+            each element of which was a 2-tuple
+                composed of node id neighbor and distance to that neighbor
+
+   THIS graph representation is as follows:
+        dictionary mapping each node id to a dict,
+            each key of this sub-dict is a neighbor node id
+            the value at each key is the distance to that neighbor
+
+    """
 def get_adjacency_list():
-    adj_list = defaultdict(list)
+    adj_list = defaultdict(dict)
     for edge in Edge.objects.all():
-        adj_list[edge.node_a.node_id].append((edge.node_b.node_id, edge.distance))
+        adj_list[edge.node_a.node_id][edge.node_b.node_id] = edge.distance
     return adj_list
 
 if __name__ == "__main__":
--- polls/graph_preparation/route_processing.py
+++ polls/graph_preparation/route_processing.py
@@ -1,6 +1,32 @@
 __author__ = 'meirfischer'
 
 from polls.models import Node, Edge
+import db_graph_2_mem
+import graph_algorithms
+
+elevs = None
+adj_list = None
+
+def ensure_set_graph_from_db():
+    global elevs, adj_list
+    if elevs is None and adj_list is None:
+        elevs = db_graph_2_mem.get_elevs()
+        adj_list = db_graph_2_mem.get_adjacency_list()
+
+"""If there is no node within distance D1 from A that has
+   an elevation in range X or if there is no node within
+   distance D1 from A that has an elevation in range Y,
+   the user will be notified and told what the max and min
+   elevations are within D1 distance from A.
+   """
+def check_nodes_in_ranges_reachable_within_distance(route_specs):
+    ensure_set_graph_from_db()
+    (D,P) = graph_algorithms.Dijkstra(adj_list, route_specs.source_node)
+    return D
+    # node within distance with elev in X
+    # node_ids = filter(lambda x, D.values())
+    # node within distance with elev in Y
+    # len(filter(,D.values()))>0
 
 #check elev ranges are allowable
 def check_inherent_specs(route_specs):
@@ -24,12 +50,26 @@ def check_node_exists_in_elev_ranges(route_specs):
         raise Exception("No node exists with elevation in range: "+
                         str(route_specs.elev_max_a)+" - "+str(route_specs.elev_max_b) + elev_message)
 
+
+"""The default value for source node is -1
+   Here we check that that value is not as
+   default since further algorithms depend
+   on it.
+   """
+def check_source_node_set(route_specs):
+    if route_specs.source_node == -1:
+        raise Exception("Please choose a starting coordinate from the drop down list")
+
 def main_route_calculator(route_specs):
     response = {};
     try:
         check_inherent_specs(route_specs)
         check_node_exists_in_elev_ranges(route_specs)
+        check_source_node_set(route_specs)
     except Exception, e:
         response["input_status"] = "BadInput"
         response["response_data"] = e.message
         return response
+    #response["input_status"] = "ActionableInput"
+    #response["response_data"] = str(check_nodes_in_ranges_reachable_within_distance(route_specs))
+    #return response
